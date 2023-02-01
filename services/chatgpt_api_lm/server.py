91d94dfb4ceaff4216b44668df8972db9bacd1b2
dilyararimovna <dilyara.rimovna@gmail.com>
2023-02-01 18:25:03 +0800

    feat: chatgpt distr

--- services/chatgpt_api_lm/server.py
+++ services/chatgpt_api_lm/server.py
@@ -1,14 +1,13 @@
 import logging
-import json
 import os
 import time
 
-import openai
 import sentry_sdk
 from flask import Flask, request, jsonify
 from sentry_sdk.integrations.flask import FlaskIntegration
 
 from pyChatGPT import ChatGPT
+# see pyChatGPT readme for info https://github.com/terry3041/pyChatGPT/blob/main/README.md
 
 
 sentry_sdk.init(dsn=os.getenv("SENTRY_DSN"), integrations=[FlaskIntegration()])
@@ -21,48 +20,32 @@ CONFIG_NAME = os.environ.get("CONFIG_NAME")
 DEFAULT_CONFIDENCE = 0.9
 ZERO_CONFIDENCE = 0.0
 
-with open(CONFIG_NAME, "r") as f:
-    chatgpt_init_params = json.load(f)
-logging.info(f"ChatGPT initialization parameters: {chatgpt_init_params}")
 
-# see pyChatGPT readme for info https://github.com/terry3041/pyChatGPT/blob/main/README.md
-chatgpt_api = ChatGPT(**chatgpt_init_params)
-logging.info("ChatGPT successfully initialized")
-
-
-def generate_responses(instruction, context, continue_last_uttr=False):
+def generate_responses(instruction, context, chatgpt_init_config, continue_last_uttr=False):
     if continue_last_uttr:
         dialog_context = instruction + "\n" + "\n".join(context)
     else:
         dialog_context = instruction + "\n" + "\n".join(context) + "\n" + "AI:"
     logger.info(f"context inside generate_responses seen as: {[dialog_context]}")
+
+    chatgpt_api = ChatGPT(**chatgpt_init_config)
+    logging.info("ChatGPT successfully initialized")
+
     response = chatgpt_api.send_message(context).get('message', "")
     return [response]
-
-
-try:
-    example_response = generate_responses(
-        "",
-        ["Question: What is the goal of SpaceX? Answer: To revolutionize space transportation. "],
-        continue_last_uttr=False,
-    )
-    logger.info(f"example response: {example_response}")
-    logger.info("openai-api-lm is ready")
-except Exception as e:
-    sentry_sdk.capture_exception(e)
-    logger.exception(e)
-    raise e
 
 
 @app.route("/respond", methods=["POST"])
 def respond():
     st_time = time.time()
     contexts = request.json.get("dialog_contexts", [])
+    chatgpt_init_params = request.json.get("chatgpt_init_params", [])
+
     try:
         responses = []
         confidences = []
-        for context in contexts:
-            outputs = generate_responses("", context)
+        for context, chatgpt_init_config in zip(contexts, chatgpt_init_params):
+            outputs = generate_responses("", context, chatgpt_init_config)
             logger.info(f"openai-api-lm result: {outputs}")
             for response in outputs:
                 if len(response) >= 3:
